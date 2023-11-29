14eb5cbc92a2d92d023c654d606016d25bb80806
Guy Turcotte <turgu666@gmail.com>
2023-11-29 16:59:29 -0500

    Added characters in support of the renderer

--- src/IBMF/IBMFHexImport.cpp
+++ src/IBMF/IBMFHexImport.cpp
@@ -7,49 +7,47 @@ enum Position { NONE, LEFT, RIGHT, CENTER };
 
 using PositionList = std::map<char32_t, Position>;
 
+// Used to modify the left position of a glyph. Mainly the RIGHT one are adjusted for now.
 PositionList positionList = {
-  { U'\U00002025', NONE  },
-  { U'\U00002014', NONE  },
-  { U'\U00002013', NONE  },
-  { U'\U0000005F', NONE  },
-  { U'\U0000005F', NONE  },
-  { U'\U00000028', NONE  },
-  { U'\U00000029', NONE  },
-  { U'\U0000007B', NONE  },
-  { U'\U0000007D', NONE  },
-  { U'\U00003014', RIGHT },
-  { U'\U00003015', LEFT  },
-  { U'\U00003010', RIGHT },
-  { U'\U00003011', LEFT  },
-  { U'\U0000300A', RIGHT },
-  { U'\U0000300B', LEFT  },
-  { U'\U00003008', RIGHT },
-  { U'\U00003009', LEFT  },
-  { U'\U0000300C', RIGHT },
-  { U'\U0000300D', LEFT  },
-  { U'\U0000300E', RIGHT },
-  { U'\U0000300F', LEFT  },
-  { U'\U0000FE51', LEFT  },
-  { U'\U0000005B', NONE  },
-  { U'\U0000005D', NONE  },
-  { U'\U0000203E', NONE  },
-  { U'\U0000203E', NONE  },
-  { U'\U0000203E', NONE  },
-  { U'\U0000203E', NONE  },
-  { U'\U0000005F', NONE  },
-  { U'\U0000005F', NONE  },
-  { U'\U0000005F', NONE  }
+    {U'\U00002025',  NONE},
+    {U'\U00002014',  NONE},
+    {U'\U00002013',  NONE},
+    {U'\U0000005F',  NONE},
+    {U'\U0000005F',  NONE},
+    {U'\U00000028',  NONE},
+    {U'\U00000029',  NONE},
+    {U'\U0000007B',  NONE},
+    {U'\U0000007D',  NONE},
+    {U'\U00003014', RIGHT},
+    {U'\U00003015',  LEFT},
+    {U'\U00003010', RIGHT},
+    {U'\U00003011',  LEFT},
+    {U'\U0000300A', RIGHT},
+    {U'\U0000300B',  LEFT},
+    {U'\U00003008', RIGHT},
+    {U'\U00003009',  LEFT},
+    {U'\U0000300C', RIGHT},
+    {U'\U0000300D',  LEFT},
+    {U'\U0000300E', RIGHT},
+    {U'\U0000300F',  LEFT},
+    {U'\U0000FE51',  LEFT},
+    {U'\U0000005B',  NONE},
+    {U'\U0000005D',  NONE},
+    {U'\U0000203E',  NONE},
+    {U'\U0000203E',  NONE},
+    {U'\U0000203E',  NONE},
+    {U'\U0000203E',  NONE},
+    {U'\U0000005F',  NONE},
+    {U'\U0000005F',  NONE},
+    {U'\U0000005F',  NONE}
 };
 
-auto IBMFHexImport::readCodePoint(std::fstream &in, char32_t &codePoint,
-                                  uint32_t &firstBytes) -> bool {
+auto IBMFHexImport::readCodePoint(std::fstream &in, char32_t &codePoint, uint32_t &firstBytes)
+    -> bool {
 
-  auto hex = [](char a) -> uint8_t {
-    return ((a >= '0') && (a <= '9')) ? a - '0' : a - 'A' + 10;
-  };
+  auto hex = [](char a) -> uint8_t { return ((a >= '0') && (a <= '9')) ? a - '0' : a - 'A' + 10; };
 
-  if (in.eof())
-    return false;
+  if (in.eof()) return false;
 
   int i;
   in >> std::hex >> i;
@@ -63,8 +61,7 @@ auto IBMFHexImport::readCodePoint(std::fstream &in, char32_t &codePoint,
     in >> b1 >> b2 >> b3 >> b4;
     firstBytes = (hex(b1) << 12) + (hex(b2) << 8) + (hex(b3) << 4) + hex(b4);
     in >> b1 >> b2 >> b3 >> b4;
-    firstBytes = (firstBytes << 16) + (hex(b1) << 12) + (hex(b2) << 8) +
-                 (hex(b3) << 4) + hex(b4);
+    firstBytes = (firstBytes << 16) + (hex(b1) << 12) + (hex(b2) << 8) + (hex(b3) << 4) + hex(b4);
     while (!in.eof() && (in.get() != '\n'))
       ;
     return true;
@@ -72,23 +69,19 @@ auto IBMFHexImport::readCodePoint(std::fstream &in, char32_t &codePoint,
   return false;
 }
 
-auto IBMFHexImport::readOneGlyph(std::fstream &in, char32_t &codePoint,
-                                 BitmapPtr bitmap, int8_t &hOffset, int8_t &vOffset,
-                                 uint16_t &advance) -> GlyphCode {
-  int i;
-  auto hex = [](char a) -> uint8_t {
-    return ((a >= '0') && (a <= '9')) ? a - '0' : a - 'A' + 10;
-  };
+auto IBMFHexImport::readOneGlyph(std::fstream &in, char32_t &codePoint, BitmapPtr bitmap,
+                                 int8_t &hOffset, int8_t &vOffset, uint16_t &advance) -> GlyphCode {
+  int  i;
+  auto hex = [](char a) -> uint8_t { return ((a >= '0') && (a <= '9')) ? a - '0' : a - 'A' + 10; };
 
   in >> std::hex >> i;
 
   char ch;
   in >> ch;
   if (ch == ':') {
-    codePoint = static_cast<char32_t>(i);
+    codePoint           = static_cast<char32_t>(i);
     GlyphCode glyphCode = toGlyphCode(codePoint);
-    if (glyphCode == NO_GLYPH_CODE)
-      goto noCode;
+    if (glyphCode == NO_GLYPH_CODE) goto noCode;
 
     std::vector<uint8_t> bytes;
     while (!in.eof() && (in.get() != '\n')) {
@@ -98,95 +91,81 @@ auto IBMFHexImport::readOneGlyph(std::fstream &in, char32_t &codePoint,
       bytes.push_back((hex(b1) << 4) + hex(b2));
     }
 
-    int byteWidth = (bytes.size() == 16) ? 1 : 2;
+    int byteWidth  = (bytes.size() == 16) ? 1 : 2;
     int byteHeight = 16;
 
-    advance = (byteWidth == 2) ? 16 : 8;
+    advance        = (byteWidth == 2) ? 16 : 8;
 
     if ((byteWidth * byteHeight) == bytes.size()) {
       int firstRow, lastRow, firstCol, lastCol;
       if (byteWidth == 1) {
         for (firstRow = 0; firstRow < 16; firstRow++) {
-          if (bytes[firstRow] != 0)
-            break;
+          if (bytes[firstRow] != 0) break;
         }
-        if (firstRow >= 16)
-          goto spaceCode;
+        if (firstRow >= 16) goto spaceCode;
         for (lastRow = 15; lastRow >= 0; lastRow--) {
-          if (bytes[lastRow] != 0)
-            break;
+          if (bytes[lastRow] != 0) break;
         }
-        if (lastRow < 0)
-          goto spaceCode; // Not really usefull...
+        if (lastRow < 0) goto spaceCode; // Not really usefull...
       } else {
         for (firstRow = 0; firstRow < 16; firstRow++) {
-          if ((bytes[firstRow << 1] != 0) || (bytes[(firstRow << 1) + 1] != 0))
-            break;
+          if ((bytes[firstRow << 1] != 0) || (bytes[(firstRow << 1) + 1] != 0)) break;
         }
-        if (firstRow >= 16)
-          goto spaceCode;
+        if (firstRow >= 16) goto spaceCode;
         for (lastRow = 15; lastRow >= 0; lastRow--) {
-          if ((bytes[lastRow << 1] != 0) || (bytes[(lastRow << 1) + 1] != 0))
-            break;
+          if ((bytes[lastRow << 1] != 0) || (bytes[(lastRow << 1) + 1] != 0)) break;
         }
-        if (lastRow < 0)
-          goto spaceCode; // Not really usefull...
+        if (lastRow < 0) goto spaceCode; // Not really usefull...
       }
 
       if (byteWidth == 1) {
         uint8_t mask = 0x80;
-        firstCol = 0;
+        firstCol     = 0;
         for (int j = 0; j < 7; j++) {
           for (int i = firstRow; i <= lastRow; i++) {
-            if (bytes[i] & mask)
-              goto end1;
+            if (bytes[i] & mask) goto end1;
           }
           mask >>= 1;
           firstCol += 1;
         }
-      end1:
-        mask = 0x01;
+end1:
+        mask    = 0x01;
         lastCol = 7;
         for (int j = 0; j < 7; j++) {
           for (int i = firstRow; i <= lastRow; i++) {
-            if (bytes[i] & mask)
-              goto end2;
+            if (bytes[i] & mask) goto end2;
           }
           mask <<= 1;
           lastCol -= 1;
         }
       } else {
         uint8_t mask = 0x80;
-        firstCol = 0;
+        firstCol     = 0;
         for (int j = 0; j < 15; j++) {
           for (int i = firstRow; i <= lastRow; i++) {
-            if (bytes[(i << 1) + (j >> 3)] & mask)
-              goto end3;
+            if (bytes[(i << 1) + (j >> 3)] & mask) goto end3;
           }
           mask >>= 1;
-          if (mask == 0)
-            mask = 0x80;
+          if (mask == 0) mask = 0x80;
           firstCol += 1;
         }
-      end3:
-        mask = 0x01;
+end3:
+        mask    = 0x01;
         lastCol = 15;
         for (int j = 15; j >= 0; j--) {
           for (int i = firstRow; i <= lastRow; i++) {
-            if (bytes[(i << 1) + (j >> 3)] & mask)
-              goto end4;
+            if (bytes[(i << 1) + (j >> 3)] & mask) goto end4;
           }
           mask <<= 1;
-          if (mask == 0)
-            mask = 0x01;
+          if (mask == 0) mask = 0x01;
           lastCol -= 1;
         }
       }
 
-    end2:
-    end4:
-      bitmap->dim = Dim(lastCol - firstCol + 1, lastRow - firstRow + 1);
-      vOffset = 14 - firstRow;
+end2:
+end4:
+      bitmap->dim   = Dim(lastCol - firstCol + 1, lastRow - firstRow + 1);
+      vOffset       = 14 - firstRow;
 
       uint8_t *buff = bytes.data() + (firstRow * byteWidth);
       for (int row = firstRow; row <= lastRow; row++) {
@@ -195,8 +174,7 @@ auto IBMFHexImport::readOneGlyph(std::fstream &in, char32_t &codePoint,
           uint8_t pixel = ((buff[col >> 3] & mask) == 0) ? 0 : 0xFF;
           bitmap->pixels.push_back(pixel);
           mask >>= 1;
-          if (mask == 0)
-            mask = 0x80;
+          if (mask == 0) mask = 0x80;
         }
         buff += byteWidth;
       }
@@ -208,7 +186,7 @@ auto IBMFHexImport::readOneGlyph(std::fstream &in, char32_t &codePoint,
 
     auto posit = positionList.find(codePoint);
     if ((posit != positionList.end()) && (posit->second == RIGHT)) {
-      hOffset = - (advance - bitmap->dim.width - 1);
+      hOffset = -(advance - bitmap->dim.width - 1);
     } else {
       hOffset = 0;
     }
@@ -216,12 +194,12 @@ auto IBMFHexImport::readOneGlyph(std::fstream &in, char32_t &codePoint,
     return glyphCode;
   } else {
 
-  noCode:
+noCode:
     while (!in.eof() && (in.get() != '\n'))
       ;
     return NO_GLYPH_CODE;
 
-  spaceCode:
+spaceCode:
     bitmap->dim = Dim(0, 0);
     bitmap->pixels.clear();
     vOffset = 0;
@@ -234,8 +212,7 @@ auto IBMFHexImport::readOneGlyph(std::fstream &in, char32_t &codePoint,
 
 // Returns true if the received character is part of the control characters nor
 // one of the space characters as defined in Unicode.
-auto IBMFHexImport::charSelected(char32_t ch, UBlocks &uBlocks,
-                                 uint32_t firstBytes) const -> bool {
+auto IBMFHexImport::charSelected(char32_t ch, UBlocks &uBlocks, uint32_t firstBytes) const -> bool {
   // Don't populate with space and non-break-space characters
   if ((ch >= 0x0021) && (ch != 0x00A0) && ((ch < 0x02000) || (ch > 0x200F)) &&
       ((ch < 0x02028) || (ch > 0x202F)) && ((ch < 0x0205F) || (ch > 0x206F)) &&
@@ -249,8 +226,7 @@ auto IBMFHexImport::charSelected(char32_t ch, UBlocks &uBlocks,
   return false;
 }
 
-auto IBMFHexImport::prepareCodePlanes(std::fstream &in, UBlocks &uBlocks)
-    -> int {
+auto IBMFHexImport::prepareCodePlanes(std::fstream &in, UBlocks &uBlocks) -> int {
 
   uint16_t glyphCode = 0;
 
@@ -258,10 +234,10 @@ auto IBMFHexImport::prepareCodePlanes(std::fstream &in, UBlocks &uBlocks)
     planes_.push_back(Plane({0, 0, 0}));
   }
 
-  bool firstSelected = false;
-  char16_t currCodePoint = 0;
-  int currPlaneIdx = 0;
-  int currCodePointBundleIdx = 0;
+  bool     firstSelected          = false;
+  char16_t currCodePoint          = 0;
+  int      currPlaneIdx           = 0;
+  int      currCodePointBundleIdx = 0;
   char32_t codePoint;
   uint32_t firstBytes;
 
@@ -271,17 +247,15 @@ auto IBMFHexImport::prepareCodePlanes(std::fstream &in, UBlocks &uBlocks)
       if (planeIdx < 4) { // Only the first 4 planes are managed
         char16_t u16 = static_cast<char16_t>(codePoint & 0x0000FFFF);
         if (!firstSelected) {
-          currCodePoint = u16;
-          currPlaneIdx = planeIdx;
+          currCodePoint     = u16;
+          currPlaneIdx      = planeIdx;
           planes_[planeIdx] = Plane{
-              .codePointBundlesIdx =
-                  static_cast<uint16_t>(codePointBundles_.size()),
-              .entriesCount = 1,
-              .firstGlyphCode = 0,
+              .codePointBundlesIdx = static_cast<uint16_t>(codePointBundles_.size()),
+              .entriesCount        = 1,
+              .firstGlyphCode      = 0,
           };
           currCodePointBundleIdx = codePointBundles_.size();
-          CodePointBundle bundle =
-              CodePointBundle({.firstCodePoint = u16, .lastCodePoint = u16});
+          CodePointBundle bundle = CodePointBundle({.firstCodePoint = u16, .lastCodePoint = u16});
           codePointBundles_.push_back(bundle);
           firstSelected = true;
         } else {
@@ -290,23 +264,21 @@ auto IBMFHexImport::prepareCodePlanes(std::fstream &in, UBlocks &uBlocks)
               planes_[idx].codePointBundlesIdx = codePointBundles_.size();
             }
             planes_[planeIdx] =
-                Plane{.codePointBundlesIdx =
-                          static_cast<uint16_t>(codePointBundles_.size()),
-                      .entriesCount = 1,
-                      .firstGlyphCode = glyphCode};
+                Plane{.codePointBundlesIdx = static_cast<uint16_t>(codePointBundles_.size()),
+                      .entriesCount        = 1,
+                      .firstGlyphCode      = glyphCode};
             currCodePointBundleIdx = codePointBundles_.size();
-            CodePointBundle bundle =
-                CodePointBundle({.firstCodePoint = u16, .lastCodePoint = u16});
+            CodePointBundle bundle = CodePointBundle({.firstCodePoint = u16, .lastCodePoint = u16});
             codePointBundles_.push_back(bundle);
             currCodePoint = u16;
-            currPlaneIdx = planeIdx;
+            currPlaneIdx  = planeIdx;
           } else {
             if (u16 == (currCodePoint + 1)) {
               codePointBundles_[currCodePointBundleIdx].lastCodePoint = u16;
             } else {
               currCodePointBundleIdx = codePointBundles_.size();
-              CodePointBundle bundle = CodePointBundle(
-                  {.firstCodePoint = u16, .lastCodePoint = u16});
+              CodePointBundle bundle =
+                  CodePointBundle({.firstCodePoint = u16, .lastCodePoint = u16});
               codePointBundles_.push_back(bundle);
               planes_[currPlaneIdx].entriesCount += 1;
             }
@@ -320,7 +292,7 @@ auto IBMFHexImport::prepareCodePlanes(std::fstream &in, UBlocks &uBlocks)
   // Completes the info of planes not used
   for (int idx = currPlaneIdx + 1; idx < 4; idx++) {
     planes_[idx].codePointBundlesIdx = codePointBundles_.size();
-    planes_[idx].firstGlyphCode = glyphCode;
+    planes_[idx].firstGlyphCode      = glyphCode;
   }
 
   return glyphCode;
@@ -358,11 +330,11 @@ auto IBMFHexImport::loadHex(std::string filename, UBlocks &uBlocs) -> bool {
     in.open(filename, std::ios::in);
 
     while (!in.eof()) {
-      char32_t codePoint;
-      auto bitmap = BitmapPtr(new Bitmap());
-      int8_t hOffset, vOffset;
+      char32_t  codePoint;
+      auto      bitmap = BitmapPtr(new Bitmap());
+      int8_t    hOffset, vOffset;
       GlyphCode glyphCode;
-      uint16_t advance;
+      uint16_t  advance;
 
       if ((glyphCode = readOneGlyph(in, codePoint, bitmap, hOffset, vOffset, advance)) !=
           NO_GLYPH_CODE) {
@@ -377,13 +349,11 @@ auto IBMFHexImport::loadHex(std::string filename, UBlocks &uBlocs) -> bool {
         char32_t firstChar = getUTF32(glyphCode);
         for (auto &ligature : ligatures) {
           if (ligature.firstChar == firstChar) {
-            GlyphCode nextGlyphCode = toGlyphCode(ligature.nextChar);
+            GlyphCode nextGlyphCode        = toGlyphCode(ligature.nextChar);
             GlyphCode replacementGlyphCode = toGlyphCode(ligature.replacement);
-            if ((nextGlyphCode != NO_GLYPH_CODE) &&
-                (replacementGlyphCode != NO_GLYPH_CODE)) {
-              glyphLigKern->ligSteps.push_back(
-                  GlyphLigStep{.nextGlyphCode = nextGlyphCode,
-                               .replacementGlyphCode = replacementGlyphCode});
+            if ((nextGlyphCode != NO_GLYPH_CODE) && (replacementGlyphCode != NO_GLYPH_CODE)) {
+              glyphLigKern->ligSteps.push_back(GlyphLigStep{
+                  .nextGlyphCode = nextGlyphCode, .replacementGlyphCode = replacementGlyphCode});
             }
           }
         }
@@ -393,25 +363,23 @@ auto IBMFHexImport::loadHex(std::string filename, UBlocks &uBlocs) -> bool {
         // ----- Glyph Info -----
 
         GlyphInfoPtr glyphInfo = GlyphInfoPtr(new GlyphInfo(GlyphInfo{
-            .bitmapWidth = static_cast<uint8_t>(bitmap->dim.width),
-            .bitmapHeight = static_cast<uint8_t>(bitmap->dim.height),
+            .bitmapWidth      = static_cast<uint8_t>(bitmap->dim.width),
+            .bitmapHeight     = static_cast<uint8_t>(bitmap->dim.height),
             .horizontalOffset = static_cast<int8_t>(hOffset),
-            .verticalOffset = static_cast<int8_t>(vOffset),
-            .packetLength =
-                static_cast<uint16_t>(bitmap->dim.width * bitmap->dim.height),
-            .advance = static_cast<FIX16>(
-                ((codePoint < 0x2E80) ||
-                         ((codePoint >= 0xA000) && (codePoint < 0xFE10)) ||
+            .verticalOffset   = static_cast<int8_t>(vOffset),
+            .packetLength     = static_cast<uint16_t>(bitmap->dim.width * bitmap->dim.height),
+            .advance          = static_cast<FIX16>(
+                ((codePoint < 0x2E80) || ((codePoint >= 0xA000) && (codePoint < 0xFE10)) ||
                          ((codePoint >= 0xFE70) && (codePoint < 0xFF00))
-                     ? (bitmap->dim.width + 1)
-                     : advance)
+                              ? (bitmap->dim.width + 1)
+                              : advance)
                 << 6),
-            .rleMetrics = RLEMetrics{.dynF = 0,
-                                     .firstIsBlack = false,
-                                     .beforeAddedOptKern = 0,
-                                     .afterAddedOptKern = 0},
+            .rleMetrics      = RLEMetrics{.dynF               = 0,
+                                          .firstIsBlack       = false,
+                                          .beforeAddedOptKern = 0,
+                                          .afterAddedOptKern  = 0},
             .ligKernPgmIndex = 0, // completed at save time
-            .mainCode = glyphCode // No composite management (for now)
+            .mainCode        = glyphCode  // No composite management (for now)
         }));
 
         face->glyphs.push_back(glyphInfo);
@@ -423,17 +391,17 @@ auto IBMFHexImport::loadHex(std::string filename, UBlocks &uBlocs) -> bool {
     // ----- Face Header -----
 
     face->header = FaceHeaderPtr(new FaceHeader({
-        .pointSize = 10,
-        .lineHeight = static_cast<uint8_t>(16),
-        .dpi = static_cast<uint16_t>(75),
-        .xHeight = static_cast<FIX16>(8 << 6),
-        .emSize = static_cast<FIX16>(10 << 6),
-        .slantCorrection = 0, // not available for FreeType
-        .descenderHeight = static_cast<uint8_t>(2),
-        .spaceSize = 5,
-        .glyphCount = static_cast<uint16_t>(glyphCount),
+        .pointSize        = 10,
+        .lineHeight       = static_cast<uint8_t>(16),
+        .dpi              = static_cast<uint16_t>(75),
+        .xHeight          = static_cast<FIX16>(8 << 6),
+        .emSize           = static_cast<FIX16>(10 << 6),
+        .slantCorrection  = 0, // not available for FreeType
+        .descenderHeight  = static_cast<uint8_t>(2),
+        .spaceSize        = 5,
+        .glyphCount       = static_cast<uint16_t>(glyphCount),
         .ligKernStepCount = 0, // will be set at save time
-        .pixelsPoolSize = 0,   // will be set at save time
+        .pixelsPoolSize   = 0, // will be set at save time
     }));
     faces_.push_back(std::move(face));
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,45 +10,43 @@ using CharsList = std::map<char32_t, uint32_t>;
 using TransList = std::map<char32_t, char32_t>;
 
 std::shared_ptr<EPubFile> ePubFile;
-CharsList charsList;
+CharsList                 charsList;
 
 TransList transList = {
-  { U'\U0000FE30', U'\U00002025'},
-  { U'\U0000FE31', U'\U00002014'},
-  { U'\U0000FE32', U'\U00002013'},
-  { U'\U0000FE33', U'\U0000005F'},
-  { U'\U0000FE34', U'\U0000005F'},
-  { U'\U0000FE35', U'\U00000028'},
-  { U'\U0000FE36', U'\U00000029'},
-  { U'\U0000FE37', U'\U0000007B'},
-  { U'\U0000FE38', U'\U0000007D'},
-  { U'\U0000FE39', U'\U00003014'},
-  { U'\U0000FE3A', U'\U00003015'},
-  { U'\U0000FE3B', U'\U00003010'},
-  { U'\U0000FE3C', U'\U00003011'},
-  { U'\U0000FE3D', U'\U0000300A'},
-  { U'\U0000FE3E', U'\U0000300B'},
-  { U'\U0000FE3F', U'\U00003008'},
-  { U'\U0000FE40', U'\U00003009'},
-  { U'\U0000FE41', U'\U0000300C'},
-  { U'\U0000FE42', U'\U0000300D'},
-  { U'\U0000FE43', U'\U0000300E'},
-  { U'\U0000FE44', U'\U0000300F'},
-  { U'\U0000FE45', U'\U0000FE51'},
-  { U'\U0000FE47', U'\U0000005B'},
-  { U'\U0000FE48', U'\U0000005D'},
-  { U'\U0000FE49', U'\U0000203E'},
-  { U'\U0000FE4A', U'\U0000203E'},
-  { U'\U0000FE4B', U'\U0000203E'},
-  { U'\U0000FE4C', U'\U0000203E'},
-  { U'\U0000FE4D', U'\U0000005F'},
-  { U'\U0000FE4E', U'\U0000005F'},
-  { U'\U0000FE4F', U'\U0000005F'}
+    {U'\U0000FE30', U'\U00002025'},
+    {U'\U0000FE31', U'\U00002014'},
+    {U'\U0000FE32', U'\U00002013'},
+    {U'\U0000FE33', U'\U0000005F'},
+    {U'\U0000FE34', U'\U0000005F'},
+    {U'\U0000FE35', U'\U00000028'},
+    {U'\U0000FE36', U'\U00000029'},
+    {U'\U0000FE37', U'\U0000007B'},
+    {U'\U0000FE38', U'\U0000007D'},
+    {U'\U0000FE39', U'\U00003014'},
+    {U'\U0000FE3A', U'\U00003015'},
+    {U'\U0000FE3B', U'\U00003010'},
+    {U'\U0000FE3C', U'\U00003011'},
+    {U'\U0000FE3D', U'\U0000300A'},
+    {U'\U0000FE3E', U'\U0000300B'},
+    {U'\U0000FE3F', U'\U00003008'},
+    {U'\U0000FE40', U'\U00003009'},
+    {U'\U0000FE41', U'\U0000300C'},
+    {U'\U0000FE42', U'\U0000300D'},
+    {U'\U0000FE43', U'\U0000300E'},
+    {U'\U0000FE44', U'\U0000300F'},
+    {U'\U0000FE45', U'\U0000FE51'},
+    {U'\U0000FE47', U'\U0000005B'},
+    {U'\U0000FE48', U'\U0000005D'},
+    {U'\U0000FE49', U'\U0000203E'},
+    {U'\U0000FE4A', U'\U0000203E'},
+    {U'\U0000FE4B', U'\U0000203E'},
+    {U'\U0000FE4C', U'\U0000203E'},
+    {U'\U0000FE4D', U'\U0000005F'},
+    {U'\U0000FE4E', U'\U0000005F'},
+    {U'\U0000FE4F', U'\U0000005F'}
 };
 
-
-
-UBlocks myUBlocks;
+UBlocks       myUBlocks;
 IBMFHexImport ibmfHexImport;
 
 void ParseFile(pugi::xml_document &doc) {
@@ -59,13 +57,11 @@ void ParseFile(pugi::xml_document &doc) {
     auto for_each(pugi::xml_node &node) -> bool override {
       if (node.type() == pugi::xml_node_type::node_pcdata) {
         const std::string data = node.value();
-        auto iter = UTF8Iterator(data);
+        auto              iter = UTF8Iterator(data);
         while (iter != data.end()) {
           char32_t ch = *iter++;
-          if ((ch > ' ') && (ch != 0xA0) &&
-              !((ch >= 0x2000) && (ch <= 0x200F)) && (ch != 0x202F) &&
-              (ch != ibmf_defs::ZERO_WIDTH_CODEPOINT) &&
-              (ch != ibmf_defs::UNKNOWN_CODEPOINT) &&
+          if ((ch > ' ') && (ch != 0xA0) && !((ch >= 0x2000) && (ch <= 0x200F)) && (ch != 0x202F) &&
+              (ch != ibmf_defs::ZERO_WIDTH_CODEPOINT) && (ch != ibmf_defs::UNKNOWN_CODEPOINT) &&
               !((ch >= 0xFFF0) && (ch <= 0xFFFF))) {
 
             auto entry = list->find(ch);
@@ -77,8 +73,8 @@ void ParseFile(pugi::xml_document &doc) {
 
             auto trans = transList.find(ch);
             if (trans != transList.end()) {
-              char32_t ch2 = trans->second;
-              auto entry = list->find(ch2);
+              char32_t ch2   = trans->second;
+              auto     entry = list->find(ch2);
               if (entry != list->end()) {
                 entry->second++;
               } else {
@@ -93,15 +89,37 @@ void ParseFile(pugi::xml_document &doc) {
 
   } walker;
 
-  walker.list = &charsList;
+  // Used to signify a link tag
+  charsList['['] = 1;
+  charsList[']'] = 1;
+
+  // Characters used for <li> tags
+  charsList['-'] = 1;
+  charsList['o'] = 1;
+
+  // Character used for mean ascii width
+  charsList['G'] = 1;
+
+  // Numbered list digits
+  charsList['0'] = 1;
+  charsList['1'] = 1;
+  charsList['2'] = 1;
+  charsList['3'] = 1;
+  charsList['4'] = 1;
+  charsList['5'] = 1;
+  charsList['6'] = 1;
+  charsList['7'] = 1;
+  charsList['8'] = 1;
+  charsList['9'] = 1;
+
+  walker.list    = &charsList;
   doc.traverse(walker);
 }
 
 void ShowCharsList() {
   int i = 0;
   for (auto &entry : charsList) {
-    std::cout << std::hex << entry.first << std::dec << ":" << entry.second
-              << " ";
+    std::cout << std::hex << entry.first << std::dec << ":" << entry.second << " ";
     i++;
     if ((i % 10) == 0) {
       std::cout << std::endl;
@@ -115,15 +133,14 @@ void ShowCharsList() {
 void BuildUBlocks() {
   uint32_t count = 0;
   uint32_t first = -1;
-  uint32_t code = -1;
-  uint32_t last = -1;
+  uint32_t code  = -1;
+  uint32_t last  = -1;
   for (auto &entry : charsList) {
     if (entry.first != code + 1) {
       if (code == -1) {
         first = code = entry.first;
       } else {
-        std::cout << std::hex << first << " .. " << code << std::dec
-                  << std::endl;
+        std::cout << std::hex << first << " .. " << code << std::dec << std::endl;
         myUBlocks.push_back(UBlockDef{first, code, ""});
         count += 1;
         code = last = first = entry.first;
@@ -142,8 +159,7 @@ void BuildUBlocks() {
   std::cout << std::endl << "MyUBlocks:" << std::endl << std::endl;
 
   for (auto &ub : myUBlocks) {
-    std::cout << std::hex << ub.first_ << " .. " << ub.last_ << std::dec
-              << std::endl;
+    std::cout << std::hex << ub.first_ << " .. " << ub.last_ << std::dec << std::endl;
   }
   std::cout << "[The End]" << std::endl;
 }
@@ -168,8 +184,7 @@ auto ScanDocument() -> bool {
 }
 
 void Usage(const char *path) {
-  std::cout << "Usage: " << path << " <HEX Font Path> <EPub file path>"
-            << std::endl;
+  std::cout << "Usage: " << path << " <HEX Font Path> <EPub file path>" << std::endl;
 }
 
 auto main(int argc, char **argv) -> int {
@@ -181,18 +196,15 @@ auto main(int argc, char **argv) -> int {
   //     return -1;
   // }
 
-  ePubFile =
-      std::make_shared<EPubFile>(argc == 3 ? argv[2] : "./V1010490321 - original.epub");
+  ePubFile = std::make_shared<EPubFile>(argc == 3 ? argv[2] : "./V1010490321 - original.epub");
 
   if (ePubFile->isOpen()) {
     log_i("File %s is open", argc == 3 ? argv[2] : "./V1010490321 - original.epub");
     if (ScanDocument()) {
-      log_i("Scan completed! Characters Count: %" PRIu32,
-            (uint32_t)charsList.size());
+      log_i("Scan completed! Characters Count: %" PRIu32, (uint32_t)charsList.size());
       ShowCharsList();
       BuildUBlocks();
-      ibmfHexImport.loadHex(argc == 3 ? argv[2] : "./unifont-15.1.04.hex",
-                            myUBlocks);
+      ibmfHexImport.loadHex(argc == 3 ? argv[2] : "./unifont-15.1.04.hex", myUBlocks);
 
       std::fstream out;
       out.open("font.ibmf", std::ios::out);
